19e8359011d412acb8e8c1e1a5e53b460138752a
Raphael Traviss <raphael@skyleafdesign.com>
2018-07-11 11:25:19 -0500

    Clarified math, architecture flaws, and added zero-length guards.

--- AlphabetSlider/AlphabetSlider/AlphabetSlider.swift
+++ AlphabetSlider/AlphabetSlider/AlphabetSlider.swift
@@ -17,7 +17,7 @@ import Foundation
 open class AlphabetSlider: UIControl {
 	
 	// MARK: public API and data model
-	open var alphabet: [String] = "12345ABCDEFG!@#$%^&*()_.XYZ".characters.map() { return String($0) } {
+  open var alphabet: [String] = "12345ABCDEFG!@#$%^&*()_.XYZ".map({ String($0) }) {
 		didSet { setNeedsDisplay() }
 	}
 	
@@ -62,12 +62,20 @@ open class AlphabetSlider: UIControl {
 	@IBInspectable open var baselineOffset: CGFloat = 0.0
 	
 	fileprivate var font: UIFont { get {
-		return UIFont(name: fontName, size: fontSize) ?? UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
-	} }
+		return UIFont(
+      name: fontName,
+      size: fontSize
+    )
+    ?? UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
+	}}
 	
 	fileprivate var focusFont: UIFont { get {
-		return UIFont(name: focusFontName, size: focusFontSize) ?? UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
-	} }
+		return UIFont(
+      name: focusFontName,
+      size: focusFontSize
+    )
+    ?? UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+	}}
 	
 	
 	
@@ -78,13 +86,10 @@ open class AlphabetSlider: UIControl {
 	// Dear Scroll View, if a user is using me, please don't update
 	// my value when your cells appear on-screen.  You may read this 
 	// variable, but please do not set it.
-	open var userIsUsing = false {
-		didSet {
-			if (userIsUsing) {
-				scrollViewIsUsing = false
-			}
-		}
-	}
+	open var userIsUsing = false { didSet {
+    guard (userIsUsing) else { return }
+    scrollViewIsUsing = false
+  }}
 
 	
 	// Dear Scroll View, I agree not to send you value changed events,
@@ -92,37 +97,43 @@ open class AlphabetSlider: UIControl {
 	// the value you set here, but I will not write to it.
 	open var scrollViewIsUsing = false
 	
-	
-	
+	// Keep track of the last-touch UIView bounds coordinate, for UIKit
+  // continueTracking method.
 	fileprivate var previousLocation = CGPoint()
 	
-	// We keep track of the precise value the user has moved
-	// their finger to.  This is used for precise tracking when the user
-	// is dragging their finger over the control.
-	fileprivate var internalValue: Double = 0.0 {
-		didSet {
-			// Enforce min/max values to protect our math.
-			let sanitizedValue = min(max(internalValue, 0), Double(alphabet.count - 1))
-			internalValue = sanitizedValue
-			
-			// Trigger an event only if we cross an integer boundary.
-			guard Int(internalValue) != Int(oldValue) else { return }
-			
-			updateIndicator()
-			
-			// Re-render the text with the new selected index.
-			setNeedsDisplay()
+  
+	// A float value that represents the integer index of the currently-focused letter.
+  // @TODO: this could be a CGFloat, to make math easier.
+	fileprivate var internalValue: Double = 0.0 { didSet {
+    // NOTE: internalValue does NOT range between 0.0 and 1.0: it ranges between 0.0 and
+    // Double(alphabet.count).  This is so that when you slide, it can convert the value
+    // to an Int, check it in a list of cached letter widths, and space the letters out
+    // correctly.
+    let max_value = max(0.0, Double(alphabet.count - 1))
+    // NOTE: once our math is more correct, we won't need this lower bounds check, because
+    // our set value on continueTracking should never fall below zero: there is some
+    // problem between UIKit's coord space and our own working coord space.
+    let sanitizedValue = max(0.0, min(internalValue, max_value))
+    
+    internalValue = sanitizedValue
+    
+    // Trigger an event only if we cross an integer boundary.
+    guard Int(internalValue) != Int(oldValue) else { return }
+    
+    updateIndicator()
+    
+    // Re-render the text with the new selected index.
+    setNeedsDisplay()
 
-			// Skip the update event if an external object has set our value;
-			// we wouldn't want to cause a loop (sending them a valueChanged notification,
-			// which in turn causes them to update their collectionview, which in
-			// turn updates our value, which in turn sends a valueChanged notification....
-			guard userIsUsing && !scrollViewIsUsing else { return }
-			
-			// Send the event notification.
-			self.sendActions(for: .valueChanged)
-		}
-	}
+    // Skip the update event if an external object has set our value;
+    // we wouldn't want to cause a loop (sending them a valueChanged notification,
+    // which in turn causes them to update their collectionview, which in
+    // turn updates our value, which in turn sends a valueChanged notification....
+    guard userIsUsing && !scrollViewIsUsing else { return }
+    
+    // Send the event notification.
+    self.sendActions(for: .valueChanged)
+  } }
 	
 	// After we draw the letters, we know how wide each one is.  Store
 	// those values here, so we can use them to properly position the
@@ -141,20 +152,12 @@ open class AlphabetSlider: UIControl {
 	}
 	
 	fileprivate func updateIndicator() {
-		// Move the highlight underneath the selected index.
-		// The indicator's distance from the left is equal to one unit of horizontal offset,
-		// centered over a letter, plus one letter width for each rouned index, which will put
-		// us right underneath the letter we just touched.
-		// We add in a little fudge factor, since workingWidthPerLetter is actually different
-		// than the actual width of the letter.
-		var fudgeFactor: CGFloat
-		if (cachedLetterWidths.count > Int(internalValue)) {
-			// @TODO: this value is too aggressive.  Ignoring it for now.
-			fudgeFactor = (workingWidthPerLetter - cachedLetterWidths[Int(internalValue)]) / 2
-		}
-		fudgeFactor = 1.0
-		
-		slideIndicator.frame.origin.x = horizonalInset + (workingWidthPerLetter / 2) + (workingWidthPerLetter * CGFloat(Int(internalValue))) + fudgeFactor
+    // This is a serious architectural flaw of AlphabetSlider: all of the characters have to be
+    // the same width.  When we say a "letter", we really mean a "label", since the labels
+    // could all be strings of varying length.  We need to stop using workingWidthPerLetter,
+    // and instead using cachedLetterWidths, every time.
+    let fudge_factor: CGFloat = 1.0
+		slideIndicator.frame.origin.x = horizonalInset + (workingWidthPerLetter / 2) + (workingWidthPerLetter * CGFloat(Int(internalValue)) + fudge_factor)
 	}
 	
 	
@@ -163,34 +166,59 @@ open class AlphabetSlider: UIControl {
 	
 	open override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
 		super.beginTracking(touch, with: event)
+    
+    // If there are no letters in the alphabet, dragging can do nothing.
+    guard alphabet.count > 0 else { return false }
+    
+    // Lock out UIScrollView events from affecting our value.
 		userIsUsing = true
-		previousLocation = touch.location(in: self)
-		let xPos = previousLocation.x
-		
-		// Convert the touched point to the drawn letter index and set our
-		// internal precise value.
-		internalValue = Double((xPos - horizonalInset - workingWidthPerLetter / 2) / workingWidthPerLetter)
-	
+
+    // There are two coordinate systems within AlphabetSlider: one is the "absolute" grid
+    // imposed by view.bounds, used by UIKit functions...
+    let absolute_x = touch.location(in: self).x
+    
+    // Converting between the two is a matter of subtracting off the inset (around the
+    // whole thing) and the extra 1/2 character spacing added on either side by
+    // workingWidthPerLetter.
+    let working_x = absolute_x - horizonalInset - workingWidthPerLetter / 2
+    
+    // Since our internalValue ranges between 0.0 and Double(alphabet.count - 1), divide
+    // the x point units by the point unit per letter, to get the letter we are on.
+		internalValue = Double(working_x / workingWidthPerLetter)
+    
+    // Set previous location, for the next UIKit continueTracking call.
+    previousLocation = touch.location(in: self)
+    
 		return true
 	}
 	
 	open override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
 		super.continueTracking(touch, with: event)
-		let location = touch.location(in: self)
-		let xPos = location.x
+    
+    // If there are no letters in the alphabet, dragging can do nothing.
+    guard alphabet.count > 0 else { return false }
+    
+		let absolute_x = touch.location(in: self).x
 		
 		// If the touch was out-of-bounds, end tracking.
-		guard xPos > horizonalInset && xPos < bounds.width - horizonalInset else { return false }
+		guard (
+      absolute_x > horizonalInset - workingWidthPerLetter / 2 &&
+      absolute_x < bounds.width - horizonalInset
+    ) else { return false }
 		
 		// Track how much the user has dragged, in global coordinate space.
-		let deltaDistance = Double(xPos - previousLocation.x)
+		let delta_x = absolute_x - previousLocation.x
 		
-		// Translate the touch delta into the actual working width of the drawn alphabet.
-		let deltaValue = (Double(alphabet.count) + 1) * deltaDistance / Double(workingWidth)
-		
-		previousLocation = location
-		internalValue += deltaValue
-		
+		// Convert the X movement (in units of dotpoints) into units of letter movement
+    // from 0.0 to CGFloat(alphabet.count - 1).
+    // NOTE: workingWidth does not take inot account the 1/2 letter width spacing on
+    // either side of the slider, so add it back in.
+		let delta_letter = delta_x * (CGFloat(alphabet.count) + 1) / workingWidth
+		internalValue += Double(delta_letter)
+    
+    // Set state for next UIKit call to continueTracking.
+    previousLocation = touch.location(in: self)
+    
 		return true
 	}
 	
@@ -225,6 +253,14 @@ open class AlphabetSlider: UIControl {
 			NSFontAttributeName: focusFont
 			] as [String : Any]
 		
+    // @TODO: the letter/label widths aren't actually cached, if we remove/rebuild
+    // them every single time.  Should be an optional value, since it only gets
+    // filled after we've rendered once.
+    // Therefore, we have two application states: init and before_render.
+    // The cache only needs to be cleared when we set the alphabet.  If fact, I
+    // wonder if we can "render" them off-screen, get the width, and fill the cache
+    // immediately?  Then we could be sure the cache would always be there, it could
+    // be non-optional, and there would be no need for multiple application states.
 		cachedLetterWidths.removeAll()
 		
 		for (index, _) in alphabet.enumerated() {
